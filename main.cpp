289e1377cea653d695983dc2bf98210adc7a3d5e
Julieen896 <juleksiewert@gmail.com>
2026-05-30 11:10:10 +0200

    Plansza nr 1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,26 @@ using namespace std;
 int main(){
 cout << "Hello World!" << endl;
 
+char plansza[10][50];
+for(int y=0;y<10;y++){
+    
+    for(int x=0;x<50;x++){
+        plansza[y][x]=' ';
+    }
+}
+for(int x=0;x<50;x++){
+    plansza[9][x]='=';
+}
+
+
+
+for(int y=0;y<10;y++){
+    cout << y;
+    for(int x=0;x<50;x++){
+        cout <<plansza[y][x];
+    }
+    cout << endl;
+}
 
 
 
