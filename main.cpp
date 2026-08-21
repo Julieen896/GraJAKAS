471eda1e3660aa49eaeb48d1a82f0c296afdbafa
Julieen896 <juleksiewert@gmail.com>
2026-08-21 13:37:04 +0200

    dodalem mechanike skoku

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h> // biblioteka zawierajaca Sleep
+#include <conio.h> // odczyt z klawiatury
 using namespace std;
 int main(){
 
@@ -9,11 +10,16 @@ int dinoY=8;
 int dinoX=4;
 char przeszkoda='#';
 int przeszkodaX=25;
+int przeszkodaY=8;
 bool GRA =true;
+bool skacze=false;
+int licznikSkoku=0;
+int wynik=0;
 
 
 
-while (true){
+
+while (GRA){
     system("cls");//wyczyszcza ekran
     for(int y=0;y<10;y++){
     
@@ -25,7 +31,7 @@ while (true){
     plansza[9][x]='=';
 }
     plansza[dinoY][dinoX]='O';
-    plansza[8][przeszkodaX]=przeszkoda;
+    plansza[przeszkodaY][przeszkodaX]=przeszkoda;
 
 
 
@@ -37,10 +43,38 @@ while (true){
     }
     cout << endl;
     }   
+    if((przeszkodaX == dinoX)&&(przeszkodaY == dinoY)){
+        cout <<"KONIEC GRY";
+        GRA=false;
+
+    }
     przeszkodaX-=1;
     if(przeszkodaX<0){
         przeszkodaX=49;
     }
+    if(_kbhit()){
+        char klawisz =_getch();
+
+        if(klawisz == ' ' && !skacze){
+            skacze=true;
+            licznikSkoku=0;
+            
+        }
+    }
+    if(skacze){
+        if(licznikSkoku<3){
+            dinoY--;
+        }
+        else if(licznikSkoku<6){
+            dinoY++;
+        }
+        else{
+            dinoY=8;
+            skacze=false;
+        }
+        licznikSkoku++;
+    }
+
     Sleep(100);//czeka 100ms
 }
 
