f95a006eb4aad7a4b500201da0682c116610f85c
Julieen896 <juleksiewert@gmail.com>
2026-08-24 11:37:31 +0200

    Napis startowy

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <windows.h> // biblioteka zawierajaca Sleep
-<<<<<<< HEAD
+
 #include <conio.h> // odczyt z klawiatury
-=======
+
 #include <conio.h> // biblioteka zawierajaca _kbhit() i _getch()
->>>>>>> 4f3427128510c84061b2fc8430b413b13edebfc3
+
 using namespace std;
 int main(){
 
@@ -18,39 +18,42 @@ plansza[11][1]=wynik;
 
 
 int przeszkodaX=25;
-<<<<<<< HEAD
-int przeszkodaY=8;
-=======
 
->>>>>>> 4f3427128510c84061b2fc8430b413b13edebfc3
+int przeszkodaY=8;
+
+
+
 bool GRA =true;
+bool start=false;
 bool skacze=false;
 int licznikSkoku=0;
-int wynik=0;
 
 
 
-<<<<<<< HEAD
 
-while (GRA){
-=======
-while (GRA){
-    
->>>>>>> 4f3427128510c84061b2fc8430b413b13edebfc3
-    system("cls");//wyczyszcza ekran
-    if (_kbhit()) {
-    int key = _getch();
-    if (key == 32 && dinoY==8 ) {  // spacja
-        if (dinoY > 0) {
-            dinoY -= 1;
-        }
+cout <<"========================================"<< endl;
+cout <<"            GOOGLE DINOZAUR"<<endl;
+cout <<"        wcisnij ENTER aby zaczac"<<endl;
+cout <<"========================================"<<endl;
+while(!start){
+if (_kbhit()){
+    int key=_getch();
+    if (key==13){
+        start=true;
     }
 }
-    if (dinoY < 8) {
-        dinoY += 1; // grawitacja
-    }
+}
+system("cls"); // czysci tekst startowy
+while (GRA){
+    
 
+    COORD pozycja;
+    pozycja.X = 0;
+    pozycja.Y = 0;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pozycja); // czysci plansze
+        
 
+    cout<<"Punkty: "<<wynik;
     for(int y=0;y<10;y++){
     
     for(int x=0;x<50;x++){
@@ -60,14 +63,14 @@ while (GRA){
     for(int x=0;x<50;x++){
     plansza[9][x]='=';
 }
-    plansza[dinoY][dinoX]='O';
-<<<<<<< HEAD
+    plansza[dinoY][dinoX]='O'; 
+
     plansza[przeszkodaY][przeszkodaX]=przeszkoda;
-=======
+
     plansza[8][przeszkodaX]=przeszkoda;
     
 
->>>>>>> 4f3427128510c84061b2fc8430b413b13edebfc3
+
 
 
 
@@ -89,7 +92,7 @@ while (GRA){
     if(przeszkodaX<0){
         przeszkodaX=49;
     }
-<<<<<<< HEAD
+
     if(_kbhit()){
         char klawisz =_getch();
 
@@ -113,19 +116,19 @@ while (GRA){
         licznikSkoku++;
     }
 
-=======
+
     
     if(przeszkodaX==dinoX && dinoY==8){
         GRA=false;
         cout << "PRZEGRALES!" << endl;
         break;
     }
->>>>>>> 4f3427128510c84061b2fc8430b413b13edebfc3
+
     Sleep(100);//czeka 100ms
 
-
-
-    wynik++;
+    cout << endl;
+    
+    wynik+=1;
 }
 
 
