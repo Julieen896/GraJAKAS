36c2794ec9d63681d5eeb7b904d97d29e7267a81
Julieen896 <juleksiewert@gmail.com>
2026-08-26 11:40:32 +0200

    podsumowanie + zagraj jeszcze raz

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <windows.h> // biblioteka zawierajaca Sleep
 
-#include <conio.h> // odczyt z klawiatury
+
 
 #include <conio.h> // biblioteka zawierajaca _kbhit() i _getch()
+#include <cstdlib> // losowanie rand() i srand()
+#include <ctime> //time(0)
 
 using namespace std;
 int main(){
+srand(time(0));
 
 
 char plansza[11][50];
@@ -14,18 +17,15 @@ int dinoY=8;
 int dinoX=4;
 char przeszkoda='#';
 int wynik=0;
-plansza[11][1]=wynik;
-
 
 int przeszkodaX=25;
-
 int przeszkodaY=8;
+int przeszkodaWysokosc = 3;
 
-
-
-bool GRA =true;
+bool GRA =false;
 bool start=false;
 bool skacze=false;
+bool chceGrac=true;
 int licznikSkoku=0;
 
 
@@ -44,92 +44,129 @@ if (_kbhit()){
 }
 }
 system("cls"); // czysci tekst startowy
-while (GRA){
-    
+while(chceGrac){
+    dinoX = 4;
+    dinoY = 8;
 
-    COORD pozycja;
-    pozycja.X = 0;
-    pozycja.Y = 0;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pozycja); // czysci plansze
+    przeszkodaX = 49;
+    przeszkodaY = 8;
+
+    GRA = true;
+    skacze = false;
+    licznikSkoku = 0;
+    wynik = 0;
+
+
+    while (GRA){
+        
+        
+        COORD pozycja;
+        pozycja.X = 0;
+        pozycja.Y = 0;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pozycja); // czysci plansze
+        cout<<"Punkty: "<<wynik;
+
+        
+        for(int y=0;y<10;y++){
+        
+        for(int x=0;x<50;x++){
+            plansza[y][x]=' ';
+        }
+    }
+        for(int x=0;x<50;x++){
+        plansza[9][x]='=';
+    }
+        plansza[dinoY][dinoX]='O'; 
+
+        
+        for (int y=0; y<przeszkodaWysokosc;y++){
+            plansza[8-y][przeszkodaX]='#';
+        }
+        plansza[8][przeszkodaX]=przeszkoda;
         
 
-    cout<<"Punkty: "<<wynik;
-    for(int y=0;y<10;y++){
-    
-    for(int x=0;x<50;x++){
-        plansza[y][x]=' ';
-    }
-}
-    for(int x=0;x<50;x++){
-    plansza[9][x]='=';
-}
-    plansza[dinoY][dinoX]='O'; 
 
-    plansza[przeszkodaY][przeszkodaX]=przeszkoda;
-
-    plansza[8][przeszkodaX]=przeszkoda;
-    
-
-
-
-
-
-
-    for(int y=0;y<10;y++){
-    cout << y;
-    for(int x=0;x<50;x++){
-        cout <<plansza[y][x];
-        
-    }
-    cout << endl;
-    }   
-    if((przeszkodaX == dinoX)&&(przeszkodaY == dinoY)){
-        cout <<"KONIEC GRY";
-        GRA=false;
-
-    }
-    przeszkodaX-=1;
-    if(przeszkodaX<0){
-        przeszkodaX=49;
-    }
-
-    if(_kbhit()){
-        char klawisz =_getch();
-
-        if(klawisz == ' ' && !skacze){
-            skacze=true;
-            licznikSkoku=0;
+        for(int y=0;y<10;y++){
+        cout << y;
+        for(int x=0;x<50;x++){
+            cout <<plansza[y][x];
             
         }
-    }
-    if(skacze){
-        if(licznikSkoku<3){
-            dinoY--;
+        cout << endl;
+        }   
+        
+        przeszkodaX-=1;
+        if(przeszkodaX<0){
+            przeszkodaX=49;
+            przeszkodaWysokosc = rand() % 3 + 1; // losowanie wysokosci kaktusa (od 1 do 3)
         }
-        else if(licznikSkoku<6){
-            dinoY++;
+
+        if(_kbhit()){
+            char klawisz =_getch();
+
+            if(klawisz == ' ' && !skacze){
+                skacze=true;
+                licznikSkoku=0;
+                
+            }
         }
-        else{
-            dinoY=8;
-            skacze=false;
+        if(skacze){
+            if(licznikSkoku<3){
+                dinoY--;
+            }
+            else if(licznikSkoku<6){
+                dinoY++;
+            }
+            else{
+                dinoY=8;
+                skacze=false;
+            }
+            licznikSkoku++;
         }
-        licznikSkoku++;
+
+
+        
+        if(przeszkodaX==dinoX && dinoY==8){
+            GRA=false;
+            break;
+            
+            
+            
+            
+        }
+        wynik+=1;
+        Sleep(100);//czeka 100ms
+
+        cout << endl;
+        
+        
     }
-
-
-    
-    if(przeszkodaX==dinoX && dinoY==8){
-        GRA=false;
-        cout << "PRZEGRALES!" << endl;
-        break;
-    }
-
-    Sleep(100);//czeka 100ms
-
+    system("cls");
+    cout << "==============================" << endl;
+    cout << "          GAME OVER" << endl;
+    cout << "       Wynik: " << wynik*10 << endl;
     cout << endl;
-    
-    wynik+=1;
-}
+    cout << "   ENTER - zagraj ponownie" << endl;
+    cout << "   ESC   - wyjdz" << endl;
+    cout << "==============================" << endl;
+    while(true)
+    {
+        if(_kbhit())
+        {
+            char klawisz = _getch();
 
+            if(klawisz == 13) // ENTER
+            {
+                break;
+            }
+
+            if(klawisz == 27) // ESC
+            {
+                chceGrac = false;
+                break;
+            }
+        }
+    }
+}
 
 }
